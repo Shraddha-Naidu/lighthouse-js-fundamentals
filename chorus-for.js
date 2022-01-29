d54b9bd55a950b4efba66ea6d1f4a02950442ded
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-01-28 21:36:17 -0700

    Add files via upload

---
+++ chorus-for.js
@@ -0,0 +1,7 @@
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
+
+//Remember to separate the start, stop, how with a semi-colon so that each statement is separated and will log accordingly.
