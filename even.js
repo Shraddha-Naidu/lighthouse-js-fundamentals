8ff7f26f72428eb066ecbacde08c4588c15beab3
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-01-29 13:24:07 -0700

    Add files via upload

---
+++ even.js
@@ -0,0 +1,15 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+//isEven takes a number as its parameter and comes back with a Boolean value as to whether the number is even.
+
+//const tenIsEven = isEven(10);
+//const elevenIsEven = isEven(11);
+
+//console.log(tenIsEven);
+//console.log(elevenIsEven);
+
+//The above code can be further simplified by nesting it into the console.log
+
+console.log(isEven(10));
+console.log(isEven(11));
